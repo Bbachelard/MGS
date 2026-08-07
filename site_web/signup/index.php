fc1f16445cbc2efc1b348cd1f11bbb63a88d8e46
Chassepeche <louisducourneau@gmail.com>
2026-08-07 22:38:55 +0200

    j'avais oublié un d

--- site_web/signup/index.php
+++ site_web/signup/index.php
@@ -20,7 +20,7 @@
         <div class="nav-box1"><a href=""><img src="../content/image/Steam_icon.webp" width="50"></a></div>
         <div class="nav-box2"><a href=""><img src="../content/image/riot-icon.png" width="50"></a></div>
         <div class="nav-box3"><a href=""><img src="../content/image/Epic_icon.webp" width="50"></a></div>
-        <div class="login"><a href="../connexion/inex.php">Connexion</a></div>
+        <div class="login"><a href="../connexion/index.php">Connexion</a></div>
     </header>
 
     <div class="page">
