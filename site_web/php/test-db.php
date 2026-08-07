e7468fda626bc00f2aad272ce97260aa2dd03720
benjamin bachelard <benjamin.bachelard.pro@gmail.com>
2026-08-07 21:09:13 +0200

    Update test-db.php

--- site_web/php/test-db.php
+++ site_web/php/test-db.php
@@ -1,18 +1,8 @@
 <?php
 
 require __DIR__ . '/../config.php';
-/*
-$stmt = $conn->prepare("INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)");
-$username = 'testuser';
-$email = 'test@example.com';
-$passwordHash = password_hash('test1234', PASSWORD_BCRYPT); 
 
-$stmt->bind_param("sss", $username, $email, $passwordHash);
-$stmt->execute();
-
-if ($stmt->error) {
-    echo "Erreur lors de l'insertion : " . $stmt->error . "\n";
-}*/
+$username = 'testuser'; 
 
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
 $stmt->bind_param("s", $username);
