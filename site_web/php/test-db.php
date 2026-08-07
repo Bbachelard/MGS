a8a36ef38304f549345e61f577690410748875e9
benjamin bachelard <benjamin.bachelard.pro@gmail.com>
2026-08-07 20:45:48 +0200

    Update test-db.php

--- site_web/php/test-db.php
+++ site_web/php/test-db.php
@@ -2,11 +2,16 @@
 
  require __DIR__ . '/../config.php';
 
+
+
+ $stmt = $conn->prepare("INSERT INTO users (username, email, password_hash)
+VALUES ('testuser', 'test@example.com', '$2y$');");
+
+
 $stmt = $conn->prepare("SELECT * FROM users");
 $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_assoc();
 
-$hash = $row['password_hash'];
-
+$hash = $row['hash'];
 echo($hash);
