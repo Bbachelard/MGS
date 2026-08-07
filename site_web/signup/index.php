5272c4b39fe831fb5b3cb60e4497a96f7d8ddd78
Chassepeche <louisducourneau@gmail.com>
2026-08-07 22:37:23 +0200

    bouton déconnexion

--- site_web/signup/index.php
+++ site_web/signup/index.php
@@ -69,9 +69,11 @@
             </div>
 
             <div class="form-group">
-                <input class="button" type="submit" value="Se connecter">
+                <div class="form-actions">
+                    <input class="button" type="submit" value="Se connecter">
+                    <a href="https://my-gamers-stats.com/signup/" class="button-2">S'inscrire</a>
+                </div>
             </div>
-            <a href="../signup/index.php">S'inscrire</a>
         </form>
     </div>
     </div>
