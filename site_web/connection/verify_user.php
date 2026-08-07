1c09c3e605b4a1563618252e658600cac28adde6
Chassepeche <louisducourneau@gmail.com>
2026-08-07 21:06:40 +0200

    ajout de fichiers

---
+++ site_web/connection/verify_user.php
@@ -0,0 +1,33 @@
+<?php 
+
+    require __DIR__ . '/../config.php';
+    session_start();
+    //parametres recus du formulaire de connexion
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    //preparation d'une requete sql propre (je ferais plus l'erreur xD)
+
+    $stmt = $conn->prepare("SELECT password_hash FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    $hash = $row['password_hash'];
+
+    $isPasswordCorrect = password_verify($password, $hash);
+
+
+    if($isPasswordCorrect == true)
+        {
+            $_SESSION["logged"] = true;
+            $_SESSION["username"] = $username;
+            header("Location: ../logged/index.php"); 
+        }
+    exit();
+
+?>
