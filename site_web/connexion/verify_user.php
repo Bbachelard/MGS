93136420dc3aff67b47e661b3b3d422075e9fb52
Chassepeche <louisducourneau@gmail.com>
2026-08-07 22:57:07 +0200

    cheppa frr j'ai faiml la

--- site_web/connexion/verify_user.php
+++ site_web/connexion/verify_user.php
@@ -1,34 +1,38 @@
-<?php 
+<?php
 
-    require __DIR__ . '/../config.php';
-    session_start();
-    //parametres recus du formulaire de connexion
+require __DIR__ . '/../config.php';
+session_start();
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+$stmt = $conn->prepare(
+    "SELECT id, password_hash FROM users WHERE username = ?"
+);
 
-    //preparation d'une requete sql propre (je ferais plus l'erreur xD)
+$stmt->bind_param("s", $username);
+$stmt->execute();
 
-    $stmt = $conn->prepare("SELECT password_hash FROM users WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
 
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-
-    $hash = $row['password_hash'];
-
-    $isPasswordCorrect = password_verify($password, $hash);
-
-
-    if($isPasswordCorrect == true)
-        {
-            $_SESSION["logged"] = true;
-            $_SESSION["user_id"]  = $row['id'];
-            $_SESSION["username"] = $username;
-            header("Location: ../logged/index.php"); 
-        }
+if ($row === null) {
+    // L'utilisateur n'existe pas
+    header("Location: index.php?error=invalid");
     exit();
+}
 
-?>
+if (password_verify($password, $row['password_hash'])) {
+
+    session_regenerate_id(true);
+
+    $_SESSION["logged"] = true;
+    $_SESSION["user_id"] = $row['id'];
+    $_SESSION["username"] = $username;
+
+    header("Location: ../logged/index.php");
+    exit();
+}
+
+header("Location: index.php?error=invalid");
+exit();
