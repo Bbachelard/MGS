eec1d9d1ba9082b3f8f4015dd4e8559dcce3b95d
Chassepeche <louisducourneau@gmail.com>
2026-08-11 15:45:26 +0200

    Update mailer.php

--- site_web/php/mailer.php
+++ site_web/php/mailer.php
@@ -21,8 +21,12 @@ function mgs_send_password_reset_email(string $toEmail, string $toUsername, stri
     $mail->Debugoutput = function ($str, $level) {
         error_log("SMTP DEBUG [$level] $str");
     };
-
+    
     $mail->isSMTP();
+
+    $mail->Timeout = 10;
+    $mail->Timelimit = 10;
+    
     $mail->Host       = $smtp_host;
     $mail->Port       = $smtp_port;
     $mail->SMTPAuth   = true;
